d951449fdc46556c816f8d150391b4ed3be6d6e2
Bruno Caldas <bruno.caldas@gmail.com>
2022-04-30 21:32:50 -0300

    feat: api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'', views.controleParceiros)
 
 urlpatterns = [
-    path('', views.getData),
+    # path('', views.getData),
+    # path('add/', views.addData)
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,30 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework import permissions
 from parceiros.models import Parceiros
 from .serializers import ParceirosSerializer
 
-@api_view(['GET'])
-def getData(request):
-    dados = Parceiros.objects.all()
-    serializer = ParceirosSerializer(dados, many=True)
-    return Response(serializer.data)
+# Easy way of controling the API
+# @api_view(['GET'])
+# def getData(request):
+#     dados = Parceiros.objects.all()
+#     serializer = ParceirosSerializer(dados, many=True)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def addData(request):
+#     serializer = ParceirosSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# Documentation says to use the following to control de API
+class controleParceiros(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Parceiros.objects.all()
+    serializer_class = ParceirosSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
