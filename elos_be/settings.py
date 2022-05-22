a10fd52dca0ac576e88d02cf6a2bae64363f944e
Bruno Caldas <bruno.caldas@gmail.com>
2022-05-22 18:02:36 -0300

    changing auth

--- elos_be/settings.py
+++ elos_be/settings.py
@@ -62,7 +62,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated'),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
