248f777110a25c369197f5ac102fbb2c18b45aa7
Bruno Caldas <bruno.caldas@gmail.com>
2022-05-22 16:43:51 -0300

    fix allowany to parceiros

--- api/views/parceiros.py
+++ api/views/parceiros.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, filters
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 
 from parceiros.models import Parceiros
