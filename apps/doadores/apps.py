a1ec8273694611e76afa5e91cc57d6f46cbb783b
VinicusGB <viniciusgomesb@gmail.com>
2022-11-19 14:13:57 -0300

    **refact(APP/DOADORES):** Adicionada melhorias: - Fiz melhorias no models adicionand novos campos; - Fiz melhorias na página do ADMIN/DOADORES/DOADOR
    originado da issue #28

--- apps/doadores/apps.py
+++ apps/doadores/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DoadoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doadores'
+    verbose_name = 'Doação'
+    verbose_name_plural = 'Doações'
